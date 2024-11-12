4145f4706a730537273c84a687dae0d8cca3e31f
ee0ee <s.leigh.c@outlook.com>
2024-11-12 10:38:51 -0800

    Moved file to virtual environment

--- is_it_raining_in_seattle.py
+++
@@ -1,9 +0,0 @@
-import urllib.parse, urllib.request, urllib.error, json
-def is_it_raining_in_seattle():
-    with urllib.request.urlopen('https://depts.washington.edu/ledlab/teaching/is-it-raining-in-seattle/') as response:
-        is_it_raining_in_seattle = response.read().decode()
-
-    if is_it_raining_in_seattle == "true":
-        return True
-    else:
-        return False
